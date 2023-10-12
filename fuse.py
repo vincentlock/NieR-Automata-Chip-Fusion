ef0b3a90b3a4c25c5ce808198a077e9b3e4c997e
Vincent Lock <vlock6@gmail.com>
2023-10-12 18:55:32 -0400

    Refactor helper methods into it's own file

--- fuse.py
+++ fuse.py
@@ -1,16 +1,7 @@
 import json
+from helper import extractChipInfo
 
 filename = "chips.json"
 
-try:
-    with open(filename, 'r') as file:
-        # content = file.read()
-        # print(content)
-        data = json.load(file)
-        # print(data)
-except FileNotFoundError:
-    raise FileNotFoundError(f"The file {filename} could not be found.")
-except PermissionError:
-    raise PermissionError(f"You do not have permission to read {filename}.")
-except Exception as e:
-    raise SystemError(f"An error occurred: {e}")
+extractChipInfo(filename)
+
---
+++ helper.py
@@ -0,0 +1,17 @@
+import json
+
+def extractChipInfo(filename):
+    try:
+        with open(filename, 'r') as file:
+            # content = file.read()
+            # print(content)
+            data = json.load(file)
+            # print(data)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"The file {filename} could not be found.")
+    except PermissionError:
+        raise PermissionError(f"You do not have permission to read {filename}.")
+    except Exception as e:
+        raise SystemError(f"An error occurred: {e}")
+    
+    return data
