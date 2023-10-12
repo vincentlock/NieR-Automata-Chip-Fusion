bbe9bf7cebf1a8ea4121738aa404b226219c7e5d
Vincent Lock <vlock6@gmail.com>
2023-10-12 19:12:00 -0400

    Add code template

--- fuse.py
+++ fuse.py
@@ -3,5 +3,13 @@ from helper import extractChipInfo
 
 filename = "chips.json"
 
-extractChipInfo(filename)
+data = extractChipInfo(filename)
 
+# parse data into some data structure (defaultdict?)
+    # error check to make sure that the chip name is valid
+    # make sure the count is valid (probably <= 99)
+    # make sure that the minimum chip values is valid (max doesn't matter since the rec will be to sell anyway)
+
+
+# iterate over each chip name
+    # brute force/greedily iterate over every level from top to botton to try to find pairings
