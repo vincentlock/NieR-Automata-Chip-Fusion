00f2869c4d2ca2f203f4d7a68ca95ca5d39bf1f2
Vincent Lock <vlock6@gmail.com>
2023-10-12 19:11:33 -0400

    Add huge set of valid chips

--- helper.py
+++ helper.py
@@ -1,5 +1,40 @@
 import json
 
+VALID_CHIP_NAMES = set([
+    "Charge-Attack",
+    "Counter",
+    "Critical-Up",
+    "Down-Attack-Up",
+    "Last-Stand",
+    "Ranged-Attack-Up",
+    "Shockwave",
+    "Weapon-Attack-Up",
+    "Anti-Chain-Damage",
+    "Auto-Heal",
+    "Damage-Absorb",
+    "Deadly-Heal",
+    "Max-HP-Up",
+    "Melee-Defense-Up",
+    "Offensive-Heal",
+    "Ranged-Defense",
+    "Reset",
+    "Resilience",
+    "Auto-Use-Item",
+    "Drop-Rate-Up",
+    "EXP-Gain-Up",
+    "Evade-Range-Up",
+    "Fast-Cooldown",
+    "Moving-Speed-Up",
+    "Overclock",
+    "Taunt-Up",
+    "Vengeance",
+    "Combust",
+    "Heal-Drops-Up",
+    "Hijack-Boost",
+    "Stun"
+])
+
+
 def extractChipInfo(filename):
     try:
         with open(filename, 'r') as file:
@@ -10,8 +45,9 @@ def extractChipInfo(filename):
     except FileNotFoundError:
         raise FileNotFoundError(f"The file {filename} could not be found.")
     except PermissionError:
-        raise PermissionError(f"You do not have permission to read {filename}.")
+        raise PermissionError(
+            f"You do not have permission to read {filename}.")
     except Exception as e:
         raise SystemError(f"An error occurred: {e}")
-    
+
     return data
